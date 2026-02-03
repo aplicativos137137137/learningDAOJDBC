9df2e77f8607e9238b92bceddbb54fdfb9222dfd
Daniel <aplicativos137137137@gmail.com>
2026-02-03 14:10:59 -0300

    Department entity class

---
+++ src/aplication/App.java
@@ -0,0 +1,12 @@
+package aplication;
+
+import model.entities.Department;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "computer");
+        System.out.print(obj);
+    }
+}
