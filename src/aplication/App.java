d44985f28e9167ae775fb458486597abfc2e7cb1
Daniel <aplicativos137137137@gmail.com>
2026-02-04 22:09:52 -0300

    Seller entity class

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -1,12 +1,18 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "computer");
-        System.out.print(obj);
+        System.out.println(obj);
+
+        Seller seller = new Seller(10, "trem", "trem@gmail.com", new Date(), 3000.00, obj);
+        System.out.print(seller);
     }
 }
