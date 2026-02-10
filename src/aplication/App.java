c8aaa61afa49f613d50e6eece97978a459c38058
Daniel <aplicativos137137137@gmail.com>
2026-02-10 13:50:09 -0300

    delete implementation

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -2,6 +2,7 @@ package aplication;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.entities.Department;
 import model.entities.Seller;
@@ -11,6 +12,8 @@ import model.DAO.DAOFactory;
 public class App {
 
     public static void main(String[] args) {
+
+        Scanner dado = new Scanner(System.in);
 
         Department obj = new Department(1, "computer");
         System.out.println(obj);
@@ -32,7 +35,7 @@ public class App {
 
         List<Seller> listFindAll = sellerDAO.findAll();
         for(Seller sellerFindAll : listFindAll) {
-            System.out.println(seller);
+            System.out.println(sellerFindAll);
         }
 
         Seller sellerTestandoOInsert = new Seller(null, "tremtestandooinsert", "tremtestandooinsert@gmail.com", new Date(), 3000.0, department);
@@ -42,7 +45,14 @@ public class App {
         Seller SellerTestandoOUpdate = sellerDAO.findById(1);
         SellerTestandoOUpdate.setBaseSalary(1500.00);
         sellerDAO.update(SellerTestandoOUpdate);
-        System.out.print("update completed");
+        System.out.println("update completed");
+ 
+        System.out.println("enter id for the delete test");
+        int id = dado.nextInt();
+        sellerDAO.deleteById(id);
+        System.out.print("delete completed");
+
+        dado.close();
 
     }
 }
